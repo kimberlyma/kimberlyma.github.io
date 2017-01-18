851cd20082f8d1d654ce825c80edb41ca3053c2d
kimberlyma <kimberlymahoney@berkeley.edu>
2017-01-17 20:32:59 -0800

    Pushing finished skeleton

--- script.js
+++ script.js
@@ -5,15 +5,23 @@ $(window).scroll(function(e){
 
 function parallax(){
   var scrolled = $(window).scrollTop();
-  $('.jumbotron').css('opacity',1-(scrolled*0.00175));
+  $('#bg').css('opacity',1-(scrolled*0.00175));
 };
 
 //Thank you relst.jquery from the jquery forum
 $(document).ready(function(){
    //timeout
-  $("li").css("display","none"); //hide
-  $("li").each(function (i){
+  $("#intro li").css("display","none"); //hide
+  $("#intro li").each(function (i){
         var me = $(this);
         setTimeout(function(){ $(me).fadeIn(1500); }, (1500 * i));
      });
   });
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
